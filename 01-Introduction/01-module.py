aaba1341214e5dc50c44367577045eebbde57d58
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-14 23:20:15 +0530

    Added imports & modules practice

---
+++ 01-Introduction/01-module.py
@@ -0,0 +1,10 @@
+'''
+To install the modules, we need to write the script in the shell:
+    pip install 'module-name'
+'''
+
+# Some imports
+import numpy 
+import pandas
+import matplotlib.pyplot
+import seaborn
