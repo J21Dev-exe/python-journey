fdeee3def52e01384525f680078ad0e4b6ac8b39
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-14 23:22:46 +0530

    Added print statements practice

---
+++ 01-Introduction/02-print_practice.py
@@ -0,0 +1,10 @@
+# How to print?
+
+print('Hello World! This is my first print statement.')
+
+# print song
+print('\nDandelions - song by Ruth B\n')
+print("Cause I'm in a field of dandelions\n Wishing on every one that you'd be mine, mine")
+print("And I see forever in your eyes\n I feel okay when I see you smile, smile")
+print("Wishing on dandelions all of the time\n Praying to God that one day you'll be mine")
+print("Wishing on dandelions all of the time, all of the time\n")
