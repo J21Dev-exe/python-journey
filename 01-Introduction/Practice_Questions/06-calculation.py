a6a7f43a0e5fd8a57ec2eb23901c364fb1ba7c38
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-15 20:40:03 +0530

    Added calculation practice

---
+++ 01-Introduction/Practice_Questions/06-calculation.py
@@ -0,0 +1,29 @@
+# Question 6:
+'''
+Take a = 25
+     b = 10
+Then, Print
+-Addition
+-Subtraction
+-Multiplication
+-Division
+-Floor Division
+-Modulus
+-Power
+'''
+
+#===================================
+# Solution:
+print('6.')
+
+a = 25
+b = 10
+
+print(f'Addition: {a + b}')
+print(f'Subtraction: {a - b}')
+print(f'Multiplication: {a * b}')
+print(f'Division: {a / b}')
+print(f'Floor Division: {a // b}')
+print(f'Modulus: {a % b}')
+print(f'Power: {a ** b}')
+#===================================
