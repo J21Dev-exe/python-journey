edbb3c408c83c4b9c4cf067a936071b30a5b61b8
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-15 20:42:52 +0530

    Added type-cast practice 01

---
+++ 01-Introduction/Practice_Questions/09-type-cast-p01.py
@@ -0,0 +1,12 @@
+# Question 9:
+'''
+Convert "50" into integer and multiply by 2
+'''
+
+#===================================
+# Solution:
+print('9.')
+
+val = '50'
+print(f'The value is: {int(val) * 2}')
+#===================================
