68fdd1a127170cf9bd548bfc1e4e1ef151c0a01c
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-15 20:37:10 +0530

    Added sequence practice

---
+++ 01-Introduction/Practice_Questions/03-sequence-print.py
@@ -0,0 +1,13 @@
+# Question 3:
+'''Print this
+Python
+    Data Science
+        Machine Learning
+'''
+
+#===================================
+# Solution:
+print("3.")
+print('Pyhton\n  Data Science\n    Machine Learning')
+
+#===================================
