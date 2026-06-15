6bc55df8fb066cf7c470726f6c1f55ecda26ca8c
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-15 20:38:08 +0530

    Added format print practice

---
+++ 01-Introduction/Practice_Questions/04-format-print.py
@@ -0,0 +1,17 @@
+# Question 4:
+'''Create Variables
+name = "Jeevan"
+age = 20
+college = "BJB"
+and print them using f-string
+'''
+
+#===================================
+# Solution:
+print('4.')
+name = "Jeevan"
+age = 21
+college = "BJB"
+
+print(f"My name is {name} of age {age} persuing bachelor's in {college}")
+#===================================
