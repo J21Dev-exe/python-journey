9b9637325d98e49927195d39c1dad492b5d82cef
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-15 20:43:42 +0530

    Added type-cast practice 02

---
+++ 01-Introduction/Practice_Questions/10-type-cast-p02.py
@@ -0,0 +1,12 @@
+# Question 10:
+'''
+Convert 100 to string and print 100 Rupees
+'''
+
+#===================================
+# Solution:
+print('10.')
+
+num = 100
+print(f'The amount is {str(num)} Rupees')
+#===================================
