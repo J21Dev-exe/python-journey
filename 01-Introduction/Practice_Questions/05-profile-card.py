b4138d8dddecfd652b9f374458cfacbc69615256
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-15 20:39:04 +0530

    Added profile card practice

---
+++ 01-Introduction/Practice_Questions/05-profile-card.py
@@ -0,0 +1,24 @@
+# Question 5:
+''' Print like this
+===== PROFILE =====
+Name   : Jeevan
+Age    : 20
+Height : 5.10
+Weight : 70
+===================
+'''
+
+#===================================
+# Solution:
+print('5.')
+
+Profile = '''===== PROFILE =====
+Name   : Jeevan
+Age    : 20
+Height : 5.10
+Weight : 70
+===================
+'''
+print(Profile)
+
+#===================================
