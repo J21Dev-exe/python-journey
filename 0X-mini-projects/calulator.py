a731b2b0f6f5b5d2357c56612778e2c4d2261b49
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-15 09:52:37 +0530

    Added calculator mini-project practice v.01

---
+++ 0X-mini-projects/calulator.py
@@ -0,0 +1,25 @@
+# Exercise - 1
+
+'''
+Create a calculator capable of performing addition, subtraction, multiplication, and division operations
+on two numbers. Your Program should format the output in a readable manner!
+'''
+
+#=========================================================================================================
+
+# Solution: 
+
+a = 20
+b = 10
+
+# addition
+print(f'The addition result of {a} & {b} is: {a + b}')
+
+# subtraction
+print(f'The subtraction result of {a} & {b} is: {a - b}')
+
+# multiplication
+print(f'The multiplication result of {a} & {b} is: {a * b}')
+
+# division
+print(f'The division result of {a} & {b} is: {a / b}')
