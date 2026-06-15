f72f3445fa723d46a10807c2dd4a76c36a22a451
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-15 20:34:58 +0530

    Added name print practice

---
+++ 01-Introduction/Practice_Questions/01-name-print.py
@@ -0,0 +1,12 @@
+# Question 1:
+''' Print this:
+My Name is Jeevan
+I am learning Python
+'''
+
+#===================================
+# Solution:
+print('1.')
+print('My name is Jeevan\nI am learning Python')
+
+#===================================
