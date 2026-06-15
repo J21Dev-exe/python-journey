5a6bbb65efeb44b11c32674d8a584fb52fb6294d
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-15 20:42:00 +0530

    Added circle area practice

---
+++ 01-Introduction/Practice_Questions/08-circle-area.py
@@ -0,0 +1,14 @@
+# Question 8:
+'''
+Find the area pf the circle
+radius = 7
+'''
+
+#===================================
+# Solution:
+print('8.')
+
+radius = 7
+print(f'Area of circle is: {3.14 * (radius ** 2)}')
+
+#===================================
