7178c3ad60e08840e27f8328d94fb184b96a1ae0
J21Dev-exe <jeevanjyotidas215@gmail.com>
2026-06-15 20:40:57 +0530

    Added rectangle area practice

---
+++ 01-Introduction/Practice_Questions/07-rectangle-area.py
@@ -0,0 +1,16 @@
+# Question 7:
+'''
+Find the area of rectangle
+length = 10
+breadth = 5
+'''
+
+#===================================
+# Solution:
+print('7.')
+
+length = 10
+breadth = 5
+
+print(f'Area of rectangle is: {length * breadth}')
+#===================================
